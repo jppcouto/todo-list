5cb6679fdea59b2b0eca46e769cb64997f0df90f
JOTA <jppcouto@live.com>
2020-10-09 02:23:45 +0100

    Insert data

--- app/Http/Controllers/Main.php
+++ app/Http/Controllers/Main.php
@@ -21,9 +21,33 @@ class Main extends Controller
         return view('new_task');
     }
 
-    public function new_task_submit(Request $r) 
+    public function new_task_submit(Request $request) 
     {
-        $texto = $r->input('text_new_task');
-        echo $texto;
+        // echo '<pre>';
+        // print_r($request->input());
+        // print_r($request->all());
+        // die();
+
+        // //solicita qual o método utilizado pelo request
+        // if(($_SERVER['REQUEST_METHOD']) == 'POST') {
+
+        // }
+        // //solicita qual o método utilizado pelo request
+        // if(!$request->isMethod('post')){
+        //     die('URL errada');
+        // }
+
+        $new_task = $request->input('text_new_task');
+        
+        //Instaciação da class Task()
+        $task = new Task();
+
+        //no objeto $task existe uma coluna chamada 'task' que será igual ao valor do input
+        $task->task = $new_task;
+
+        //inserção do registo na BD
+        $task->save();
+
+        echo 'Done!';
     }
 }
